3e8b688204f946c01eccad7b12879b8ff595de21
Ghadi Kashgari <gh906706@dal.ca>
2018-02-10 04:00:14 -0400

    updates

--- app/src/main/java/com/example/ghadikashgari/validator_a2/passwordValidator.java
+++ app/src/main/java/com/example/ghadikashgari/validator_a2/passwordValidator.java
@@ -1,14 +1,11 @@
 package com.example.ghadikashgari.validator_a2;
 
 /**
- * Created by GhadiKashgari on 2018-02-10.
- */
-
-/**
  * Created by GhadiKashgari on 2018-02-07.
  * B00674751
  */
-
+import java.lang.String;
+import java.lang.Character;
 public class passwordValidator {
     private String password;
 
@@ -46,19 +43,73 @@ public class passwordValidator {
         boolean len = passwordLength(password);
         boolean compare = compare(password);
 
-        //true and true = true
+       
         if (len && compare){
             return 2;
         }
 
-        // (true and false) or (false and true)
+
         else if ((len && !compare) || (!len && compare)){
             return 1;
         }
 
-        // false and false = false
+
         else {
             return 0;
+        }
+    }
+    //Rule1: Password must have at least two digits(numbers).
+    public boolean numberOfDigits(String password){
+        int count = 1;
+        char ch;
+        for (int i =0; i<password.length()-1; i++){
+            ch = password.charAt(i);
+            if (Character.isDigit(ch)){
+                count++;
+            }
+
+        }
+        if (count < 2){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    //Rule2: Password must have at least 1 special character (e.g. -, _, ,, etc.)
+    public boolean numberOfCharac(String password){
+        int count =0;
+        char  ch;
+        for (int i =0; i<password.length()-1; i++){
+            ch = password.charAt(i);
+            if (!Character.isLetterOrDigit(ch)){
+                count ++;
+            }
+        }
+        if (count >= 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    //Rule3:Password must contain at least one upper case letter
+    public static boolean numberOfUppercase(String password){
+        int count =0;
+        char ch;
+        for (int i=0; i<password.length()-1; i++){
+            ch = password.charAt(i);
+            if (Character.isUpperCase(ch)){
+                count ++;
+            }
+        }
+        if (count >= 1){
+            return true;
+        }
+        else {
+            return false;
         }
     }
 }
